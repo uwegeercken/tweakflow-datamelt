e883e636593159939b5ca406695bb4ba7487a893
uwegeercken <uwe.geercken@web.de>
2021-03-13 14:45:06 +0100

    more tests for soundex

--- src/test/java/com/datamelt/tweakflow/coords/SoundexTest.java
+++ src/test/java/com/datamelt/tweakflow/coords/SoundexTest.java
@@ -19,4 +19,52 @@ public class SoundexTest
 
         assertEquals("H516", value.string());
     }
+
+    @Test
+    void testCodeEmptyString() {
+        Soundex.code c = new Soundex.code();
+        Value value = c.call(context, Values.make(""));
+
+        assertEquals(Values.NIL, value);
+    }
+
+    @Test
+    void testEqualsTrue() {
+        Soundex.equals e = new Soundex.equals();
+        Value value = e.call(context, Values.make("Hamburg"), Values.make("Hamburg"));
+
+        assertEquals(Values.TRUE, value);
+    }
+
+    @Test
+    void testEqualsTrue2() {
+        Soundex.equals e = new Soundex.equals();
+        Value value = e.call(context, Values.make("Hamburg"), Values.make("Himburrggg"));
+
+        assertEquals(Values.TRUE, value);
+    }
+
+    @Test
+    void testEqualsFalse() {
+        Soundex.equals e = new Soundex.equals();
+        Value value = e.call(context, Values.make("Banane"), Values.make("Zalando"));
+
+        assertEquals(Values.FALSE, value);
+    }
+
+    @Test
+    void testEqualsFalseWithNil() {
+        Soundex.equals e = new Soundex.equals();
+        Value value = e.call(context, Values.NIL, Values.make("Torte"));
+
+        assertEquals(Values.FALSE, value);
+    }
+
+    @Test
+    void testEqualsFalseWithBothNil() {
+        Soundex.equals e = new Soundex.equals();
+        Value value = e.call(context, Values.NIL, Values.NIL);
+
+        assertEquals(Values.FALSE, value);
+    }
 }
